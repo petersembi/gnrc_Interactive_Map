810fd1b4f7d1483900af2ec2fb2b905c9284cf83
petersembi <petersembi@gmail.com>
2023-02-04 09:04:12 +0300

    initial commit

---
+++ data/markers.js
@@ -0,0 +1,20 @@
+var markers = [
+    {
+        country_name : 'Burundi',
+        contact_person : 'Ms. Sandrine Nkurunziza',
+        contact_description : 'Contact Person, GNRC Burundi',
+         latitude : '3.3731',
+         longitude : '29.9189', 
+         img_url : 'https://gnrc.net/wp-content/uploads/2022/10/Sandrine_Nkurunziza.jpg'
+
+    },
+    { 
+        country_name : 'Cameroon',
+        contact_person : 'Mr. Alexander Gwanvalla',
+        contact_description : 'Contact Person, GNRC Cameroon.',
+         latitude : '7.3697',
+         longitude : '12.3547', 
+         img_url : 'https://gnrc.net/wp-content/uploads/2022/10/Alexander_Gwanvalla.jpg'
+
+    },
+];
